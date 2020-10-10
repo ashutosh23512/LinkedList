e0d44894bf557521b7c90e0f6f15fa6a350209cf
ASHUTOSH <ashutosh@192.168.1.3>
2020-10-10 12:52:34 +0530

    [Ashutosh]Added:Created Linked List

--- src/test/java/com/datastructures/DataStrucTest.java
+++ src/test/java/com/datastructures/DataStrucTest.java
@@ -6,6 +6,11 @@ public class DataStrucTest {
 
 	@Test
 	public void LinkedList() {
+		DataStruc<Integer> firstnode = new DataStruc<Integer>(56);
+		DataStruc<Integer> secondnode = new DataStruc<Integer>(30);
+		DataStruc<Integer> thirdnode = new DataStruc<Integer>(70);
+
+		LinkedList list = new LinkedList();
 
 	}
 }
