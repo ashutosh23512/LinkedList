5ac6dad0e6b3fc1f261fbd16d5f55d1f701476df
ASHUTOSH <ashutosh@192.168.1.2>
2020-10-13 15:44:37 +0530

    [Ashutosh]Added:Remove Key

--- src/test/java/com/datastructures/MyHashMapTestPara.java
+++ src/test/java/com/datastructures/MyHashMapTestPara.java
@@ -21,6 +21,9 @@ public class MyHashMapTestPara {
 		}
 		System.out.println(mylistpara);
 		int freq = mylistpara.get("paranoid");
+		mylistpara.remove("are");
+		System.out.println(mylistpara);
+		Assert.assertEquals(3, freq);
 
 	}
 
