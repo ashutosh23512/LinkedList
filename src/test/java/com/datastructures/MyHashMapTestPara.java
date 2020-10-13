a17a6ebfd3f36ca825c5212a0d3ce97ae22175c2
ASHUTOSH <ashutosh@192.168.1.2>
2020-10-13 15:42:59 +0530

    [Ashutosh]Added:Check Frequency Para

--- src/test/java/com/datastructures/MyHashMapTestPara.java
+++ src/test/java/com/datastructures/MyHashMapTestPara.java
@@ -4,6 +4,24 @@ import org.junit.*;
 
 public class MyHashMapTestPara {
 
+	@Test
+	public void hashmapparatest() {
+		String sen = "Paranoids are not paranoid beacuse they are paranoid but "
+				+ "because they keep putting themselves deliberately into " + "paranoid avoidable situations ";
 
+		MyHashMapPara<String, Integer> mylistpara = new MyHashMapPara<>();
+		String[] words = sen.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = mylistpara.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			mylistpara.add(word, value);
+		}
+		System.out.println(mylistpara);
+		int freq = mylistpara.get("paranoid");
+
+	}
 
 }
