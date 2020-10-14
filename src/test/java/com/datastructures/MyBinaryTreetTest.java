a03461fbf7aed13be59b4fa9c46732085c7c53a5
ASHUTOSH <ashutosh@192.168.1.2>
2020-10-14 12:48:03 +0530

    [Ashutosh]Added:Full Binary Search Tree

--- src/test/java/com/datastructures/MyBinaryTreetTest.java
+++ src/test/java/com/datastructures/MyBinaryTreetTest.java
@@ -12,9 +12,34 @@ public class MyBinaryTreetTest {
 		mbt.add(56);
 		mbt.add(30);
 		mbt.add(70);
+
 		int size = mbt.getsize();
 		System.out.println(size);
 		Assert.assertEquals(3, size);
+
+	}
+
+	@Test
+	public void MyTreeTestFull() {
+
+		MyBinaryTree<Integer> mbt = new MyBinaryTree<>();
+		mbt.add(56);
+		mbt.add(30);
+		mbt.add(70);
+		mbt.add(22);
+		mbt.add(40);
+		mbt.add(11);
+		mbt.add(3);
+		mbt.add(16);
+		mbt.add(60);
+		mbt.add(95);
+		mbt.add(65);
+		mbt.add(63);
+		mbt.add(67);
+
+		int size = mbt.getsize();
+		System.out.println(size);
+		Assert.assertEquals(13, size);
 
 	}
 
